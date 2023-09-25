a2bcdf8c8c3c8c46f85bf794341c4e06e28c5d69
Yanggz <350926623@qq.com>
2023-09-25 20:17:25 +0800

    feat: unlock

---
+++ components/pcReader/slideOperate/Qrcode.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+import { IChapterListItem } from "@/typings/book.interface";
+import styles from '@/components/pcReader/slideOperate/Qrcode.module.scss';
+import QRCode from "qrcode.react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { Toast } from "antd-mobile";
+
+interface IProps {
+  url: string;
+  chapterList: IChapterListItem[];
+}
+
+const Qrcode: FC<IProps> = ({ url}) => {
+
+  return <div className={styles.qrCodeBox}>
+    <p className={styles.intro}>上点众阅读APP，体验流畅阅读</p>
+    <QRCode value={url} className={styles.qrCode}/>
+    <CopyToClipboard text={url} onCopy={() => {
+      Toast.show('复制成功！')
+    }}>
+      <div className={styles.qrCodeLink}>复制二维码链接</div>
+    </CopyToClipboard>
+  </div>
+}
+export default Qrcode;
--- components/reader/chapterUnlock/ChapterUnlock.tsx
+++ components/reader/chapterUnlock/ChapterUnlock.tsx
@@ -1,17 +1,7 @@
-import React, { FC, useMemo } from "react";
+import React, { FC } from "react";
 import styles from '@/components/reader/chapterUnlock/ChapterUnlock.module.scss'
-import Link from "next/link";
-import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { INetChapterDetailRes } from "typings/book.interface";
-import { useAppSelector } from "@/store";
-import useHiveLog from "@/hooks/useHiveLog";
-import { isIos } from "@/utils/ownOs";
-import ClientConfig from "@/client.config";
-import { useAppContext } from "@/components/layout";
 import { IBookItem } from "@/typings/home.interface";
-import Image from "next/image";
-
-const androidLink = ClientConfig.android.link;
 
 interface IProps {
   chapterInfo: INetChapterDetailRes;
@@ -19,47 +9,9 @@ interface IProps {
 }
 
 const ChapterUnlock: FC<IProps> = ({ bookInfo, chapterInfo }) => {
-  const [appState] = useAppContext();
-  const copyText = useAppSelector(state => state.hive.copyText);
-  const clipboard = useAppSelector(state => state.hive.clipboard);
-  const HiveLog = useHiveLog();
 
-  const iosLink = useMemo(() => {
-    if (Reflect.has(ClientConfig.ios, 'deeplink')) {
-      return Reflect.get(ClientConfig.ios, 'deeplink') + copyText
-    } else {
-      return ClientConfig.ios.link
-    }
-  }, [copyText]);
-
-  return <div
-    style={{ background: `linear-gradient(180deg, transparent 0%, ${appState.background} 20%, ${appState.background})` }}
-    className={styles.unlockWrap}>
-    <div className={styles.logoBox}>
-      <Image
-        className={styles.logoIcon}
-        width={144}
-        height={144}
-        src={'/images/logo.png'}
-        alt={''}
-      />
-      <p>解鎖更多章節，馬上下載“{ClientConfig.name}”APP</p>
-      <p>獲取此書更多精彩小說內容</p>
-    </div>
-    <Link rel={'nofollow'} href={isIos(clipboard.ua) ? iosLink : androidLink}>
-      <CopyToClipboard text={copyText} onCopy={() => {
-        // HiveLog.trackDownload('BookReaderDownloadButton_Click', {
-        //   bookId: bookInfo.bookId,
-        //   bookName: bookInfo.bookName,
-        //   chapterId: chapterInfo.id,
-        //   chapterName: chapterInfo.chapterName,
-        // })
-        // netIpUa(copyText)
-      }}>
-        <div className={styles.iconBox}>立即下載</div>
-      </CopyToClipboard>
-    </Link>
-
+  return <div className={styles.unlockBox}>
+    <button className={styles.downloadBtn}>打开点众阅读APP体验流畅阅读</button>
   </div>
 }
 
